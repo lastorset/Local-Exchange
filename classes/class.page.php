8eb48d09788fa9ffe3a8bd3bff6239da05028700
Leif Arne Storset <leifarne@storset.net>
2011-05-28 21:11:42 +0200

    Serve pages in UTF-8
    This should be unproblematic since the previous encoding was ISO-8859-1.
    All or most characters used in English are the same in the two encodings.

--- classes/class.page.php
+++ classes/class.page.php
@@ -53,7 +53,7 @@ class cPage {
 	<HEAD>
 		<link rel="stylesheet" href="http://{$c['HTTP_BASE']}/{$c['SITE_STYLESHEET']}" type="text/css"></link>
 		<link rel="shortcut icon" href="http://{$c['IMAGES_PATH']}{$c['FAVICON']}" />
-		<meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1">
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 		<meta name="description" content="{$this->page_title}">
 		<meta name="keywords" content="{$this->keywords}">
 		<title>{$c['PAGE_TITLE_HEADER']}$title</title>
