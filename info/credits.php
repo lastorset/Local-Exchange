ec2b281d1710d05ace84728340377a3e257055af
Leif Arne Storset <leifarne@storset.net>
2011-05-30 23:37:14 +0200

    Update credits
    Now covers all four versions and a link to GitHub.

--- info/credits.php
+++ info/credits.php
@@ -2,19 +2,35 @@
 
 include_once("../includes/inc.global.php");
 $p->site_section = LISTINGS;
-$p->page_title = "Credits: Local Exchange UK Ver. ".LOCALX_VERSION;
+$p->page_title = "Credits: Local Exchange Oslo Ver. ".LOCALX_VERSION;
 
 print $p->MakePageHeader();
 print $p->MakePageMenu();
 print $p->MakePageTitle();
 
 ?>
-<p> <b>This website is based on the one designed for Fourth Corner Exchange, in 
+The software has been built in several stages, each built on the previous version. All versions are most likely still maintained. This list is in reverse chronological order:
+
+<h3>Oslo fork</h3>
+
+<p>Project led by Douwe Beerda. Software development by Leif Arne Storset.
+
+<p>Source code available at <a href=https://github.com/lastorset/Local-Exchange/>GitHub</a>.
+
+<h3>Internationalized version</h3>
+
+Developed by Evert Jan Klein Velderman.
+
+<h3>UK version</h3>
+<p>This website is based on the one designed for Fourth Corner Exchange, in 
   Bellingham, Washington State, USA, as per the credits below, and was developed, 
   with permission, for use in the UK, by Mary and Progga under the auspices of 
-  LETSlink London during summer 2007. Enhancements developed by <a href="http://cdmweb.co.uk" target="_blank">Chris McDonald</a> and <a href="http://rofo.co.uk" target="_blank">Rob Follett</a> of <a href="http://falmouthlets.org.uk" target="_blank">Falmouth LETS</a> and added for UK version 1.0 Nov 2008 - June 2009</b></p> 
-<p><b>Further software developments under the auspices of LETSlink UK are being recorded 
-  on <a href="http://www.cxss.info" target"_blank">www.cxss.info</a></b></p>
+  LETSlink London during summer 2007. Enhancements developed by <a href="http://cdmweb.co.uk" target="_blank">Chris McDonald</a> and <a href="http://rofo.co.uk" target="_blank">Rob Follett</a> of <a href="http://falmouthlets.org.uk" target="_blank">Falmouth LETS</a> and added for UK version 1.0 Nov 2008 - June 2009
+</p>
+<p>Further software developments under the auspices of LETSlink UK are being recorded 
+  on <a href="http://www.cxss.info" target"_blank">www.cxss.info</a></p>
+
+<h3>Original version</h3>
 <p> PHP development for the <A HREF="http://sourceforge.net/projects/local-exchange/" target="_blank">Local 
   Exchange</A> system was by Calvin Priest. Original informational content was 
   by Francis Ayley &amp; Cheryl Niles. Graphics were contributed by Calvin Priest from 
