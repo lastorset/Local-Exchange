6f1dc2bfd4f4a73ea43792c9ed76b8611a5f55fe
Leif Arne Storset <leifarne@storset.net>
2012-11-29 21:59:15 +0100

    index.php shows HTML using PHP tags
    This can be used in the global scope, not in functions, so it's
    best left as a single example.

--- index.php
+++ index.php
@@ -6,22 +6,26 @@ $p->site_section = 0;
 print $p->MakePageHeader();
 print $p->MakePageMenu();
 
-print "<div class=homepage>";
+?>
+<div class=homepage>
 
-$c = get_defined_constants();
-print <<<HTML
 <div class=logoheader>
 	<img src='images/localx_home.png'>
 	<div>
-		<h1>{$c['SITE_HOME_TITLE']}</h1>
-		<div class=tagline>{$c['SITE_HOME_TAGLINE']}</div>
+		<h1><?= SITE_HOME_TITLE ?></h1>
+		<div class=tagline><?= SITE_HOME_TAGLINE ?></div>
 	</div>
 </div>
-HTML;
+<h2><?= _("Why have a Local, Sustainable Currency?") ?></h2>
+<ul>
+<li><?= _("Enhance Your Prosperity") ?></li>
+<li><?= _("Build a Sustainable Community") ?></li>
+<li><?= _("Utilize Your Talents") ?></li>
+<li><?= _("Nurture the Unique Quality of Your Hometown") ?></li>
+<li><?= _("Have Fun") ?></li></ul>
 
-print "<h2>" ._("Why have a Local, Sustainable Currency?"). "</h2><ul><li>" ._("Enhance Your Prosperity"). "</li><li>" ._("Build a Sustainable Community"). "</li><li>" ._("Utilize Your Talents"). "</li><li> " ._("Nurture the Unique Quality of Your Hometown"). "</li><li>" ._("Have Fun"). "</li></ul>";
-
-print "</div>";
+</div>
+<? ;
 
 print $p->MakePageFooter();
 
