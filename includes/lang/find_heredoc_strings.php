84df6468a41a7375dc852f427704819211c362c6
Leif Arne Storset <leifarne@storset.net>
2012-11-30 19:07:13 +0100

    Warn if some strings are not using the heredoc workaround
    Also warn if no arguments are given
    Make gettext script abort if command did not succeed

--- includes/lang/find_heredoc_strings.php
+++ includes/lang/find_heredoc_strings.php
@@ -4,6 +4,10 @@
  */
 //$dirs = array(".", "includes", "classes");
 $dirs = array_slice($argv, 1);
+if (count($dirs) == 0) {
+	fwrite(STDERR, $argv[0] .": Please provide at least one directory name\n");
+	die;
+}
 $out = STDOUT;
 
 fwrite($out, "<?php\n// Gettext strings extracted from heredocs\n");
@@ -25,18 +29,27 @@ function php_files_in_dir($dir) {
 }
 
 function heredocs($filename, $out) {
+	global $argv;
 	$file = file_get_contents($filename);
 	if (preg_match_all("/<<<(\w+)(.*\n)*?\\1/", $file, $matches)) {
 		fwrite($out, "\n// $filename:\n");
 		$n = 0;
-		foreach($matches[0] as $match)
+		foreach($matches[0] as $match) {
 			if (preg_match_all('/{\$(_\("(.|\n)*?"\))}/', $match, $string_matches))
 				foreach($string_matches[1] as $string) {
 					fwrite($out, "$string;\n");
 					$n++;
 				}
+			if (preg_match_all('/[^{][^\$](_\("(.|\n)*?"\))[^}]/', $match, $string_matches)) {
+				fwrite(STDERR, "in $filename:\n");
+				fwrite(STDERR, "Warning: the following strings were not using the heredoc workaround:\n");
+				foreach($string_matches[1] as $string) {
+					fwrite(STDERR, "*** $string;\n");
+				}
+			}
+		}
 		if ($n == 0)
-			fwrite($out, "// (heredocs found, but no gettext strings)\n");
+			fwrite($out, "// (heredocs found, but no valid gettext strings)\n");
 	}
 }
 
