de422c9711cff9ea0689a1cd8880f789e797a054
Leif Arne Storset <leifarne@storset.net>
2013-02-23 12:06:22 +0100

    Fix alignment on listing page

--- listing_detail.php
+++ listing_detail.php
@@ -9,11 +9,14 @@ include_once("classes/class.listing.php");
 
 $listing = new cListing();
 $listing->LoadListing($cDB->UnEscTxt($_GET['title']), $_GET['member_id'], substr($_GET['type'],0,1));
-$output = $listing->DisplayListing();
+$output = "<article class=listing>"
+	. $listing->DisplayListing();
 if ($cUser->IsLoggedOn())
 	$output .= $listing->member->DisplayMember();
 else
 	$output .= "<p>". _("You may see more details if you sign up and log in.");
+
+$output .= "</article>";
 
 $p->DisplayPage($output);
 
