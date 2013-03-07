50cf500d447d893297e9823097bd57cd17a2b630
Leif Arne Storset <leifarne@storset.net>
2013-03-07 08:08:25 +0100

    Make login/signup links when viewing listing

--- listing_detail.php
+++ listing_detail.php
@@ -4,6 +4,9 @@ include_once("includes/inc.global.php");
 
 $p->site_section = LISTINGS;
 
+if (isset($_GET['log_me_in']))
+	$cUser->MustBeLoggedOn();
+
 include_once("classes/class.listing.php");
 
 $listing = new cListing();
@@ -20,7 +23,11 @@ $output = "<article class=listing>"
 if ($cUser->IsLoggedOn())
 	$output .= $listing->member->DisplayMember();
 else
-	$output .= "<p>". _("You may see more details if you sign up and log in.");
+	// TODO: Ideally, people should be returned to this listing after creating an account.
+	// Translation hint: %1$s and the like are link tags and must be left as they are.
+	$output .= "<p>". sprintf(_('You may see more details if you %1$s sign up%2$s or %3$s log in%4$s.'),
+		SELF_REGISTRATION ? "<a href=member_create.php>" : "", SELF_REGISTRATION ? "</a>" : "",
+		"<a href={$listing->GetUrl()}&amp;log_me_in>", "</a>");
 
 $output .= "</article>";
 
